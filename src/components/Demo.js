9b8c30333710670dd346513d0b1e497863ac044c
VeroniqueDM <veronika.dmiteva@gmail.com>
2022-10-29 17:53:33 +0200

    added responsive navigation design

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,44 +1,166 @@
-export default function Header() {
+
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
     return (
-       <header className="slider-main">
-       <div id="carouselExampleIndicators" className="carousel slide carousel-fade" data-ride="carousel">
-           <ol className="carousel-indicators">
-              <li data-bs-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-              <li data-bs-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-bs-target="#carouselExampleIndicators" data-slide-to="2"></li>
-           </ol>
-           <div className="carousel-inner" role="listbox">
-              {/* <!-- Slide One - Set the background image for this slide in the line below --> */}
-              <div className="carousel-item active" style={{backgroundImage: `url('images/slider-01.jpg')`}}>
-                 <div className="carousel-caption d-none d-md-block">
-                    <h3>Welcome to N & LW Lawn Care</h3>
-                    <p>A Great Theme For Garden Lawn Care</p>
-                 </div>
-              </div>
-              {/* <!-- Slide Two - Set the background image for this slide in the line below --> */}
-              <div className="carousel-item" style={{backgroundImage: `url('images/slider-02.jpg')`}}>
-                 <div className="carousel-caption d-none d-md-block">
-                    <h3>Beautiful Garden</h3>
-                    <p>A Great Theme For Garden Lawn Care</p>
-                 </div>
-              </div>
-              {/* <!-- Slide Three - Set the background image for this slide in the line below --> */}
-              <div className="carousel-item" style={{backgroundImage: `url('images/slider-03.jpg')`}}>
-                 <div className="carousel-caption d-none d-md-block">
-                    <h3>Welcome to N & LW Lawn Care</h3>
-                    <p>A Great Theme For Garden Lawn Care</p>
-                 </div>
-              </div>
-           </div>
-           <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-               <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-               <span className="sr-only">Previous</span>
-           </a>
-           <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-               <span className="carousel-control-next-icon" aria-hidden="true"></span>
-               <span className="sr-only">Next</span>
-           </a>
-       </div>
-   </header>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+            <div className="container">
+                <Navbar.Brand className="navbar-brand" to="/">
+                    {/* <a className="navbar-brand" href="index.html"> */}
+                    <img className="nav-logo"
+                        src="images//footer-logo.png"
+                        alt="logo"
+                    />
+                    {/* </a> */}
+                </Navbar.Brand>
+                <button
+                    className="navbar-toggler navbar-toggler-right"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive"
+                    aria-expanded="true"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="fas fa-bars"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Nav.Link className="nav-link active" to="/">
+                                Home
+                            </Nav.Link>
+                        </li>
+                        <li className="nav-item">
+                            <Nav.Link className="nav-link" to="/about">
+                                About
+                            </Nav.Link>
+                        </li>
+                        <li className="nav-item">
+                            <Nav.Link className="nav-link" to="/services">
+                                Services
+                            </Nav.Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <NavDropdown
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                id="navbarDropdownPortfolio"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Portfolio
+                            </NavDropdown>
+                            <div
+                                className="dropdown-menu dropdown-menu-right"
+                                aria-labelledby="navbarDropdownPortfolio"
+                            >
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="portfolio-1-col.html"
+                                >
+                                    1 Column Portfolio
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="portfolio-2-col.html"
+                                >
+                                    2 Column Portfolio
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="portfolio-3-col.html"
+                                >
+                                    3 Column Portfolio
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="portfolio-4-col.html"
+                                >
+                                    4 Column Portfolio
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="portfolio-item.html"
+                                >
+                                    Single Portfolio Item
+                                </NavDropdown.Item>
+                            </div>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <NavDropdown
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                id="navbarDropdownBlog"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Blog
+                            </NavDropdown>
+                            <div
+                                className="dropdown-menu dropdown-menu-right"
+                                aria-labelledby="navbarDropdownBlog"
+                            >
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="blog-home-1.html"
+                                >
+                                    Blog Home 1
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="blog-home-2.html"
+                                >
+                                    Blog Home 2
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="blog-post.html"
+                                >
+                                    Blog Post
+                                </NavDropdown.Item>
+                            </div>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <NavDropdown
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                id="navbarDropdownBlog"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Pages
+                            </NavDropdown>
+                            <div
+                                className="dropdown-menu dropdown-menu-right"
+                                aria-labelledby="navbarDropdownBlog"
+                            >
+                                <NavDropdown.Item className="dropdown-item" href="faq.html">
+                                    FAQ
+                                </NavDropdown.Item>
+                                <NavDropdown.Item className="dropdown-item" href="404.html">
+                                    404
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
+                                    className="dropdown-item"
+                                    href="pricing.html"
+                                >
+                                    Pricing Table
+                                </NavDropdown.Item>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <Nav.Link className="nav-link" to="/contact">
+                                Contact
+                            </Nav.Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
- }
+}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,164 +1,79 @@
-import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function Navigation() {
-    return (
-        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
-            <div className="container">
-                <Link className="navbar-brand" to="/">
-                    {/* <a className="navbar-brand" href="index.html"> */}
-                    <img className="nav-logo"
+function Navigation() {
+  return (
+    
+    <Navbar bg="light" expand="lg" className="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
+      <Container className="container">
+        <Navbar.Brand className="navbar-brand" to="/"> 
+        <img className="nav-logo"
                         src="images//footer-logo.png"
                         alt="logo"
                     />
-                    {/* </a> */}
-                </Link>
-                <button
-                    className="navbar-toggler navbar-toggler-right"
+        </Navbar.Brand>
+        <Navbar.Toggle className="navbar-toggler navbar-toggler-right"
                     type="button"
                     data-toggle="collapse"
                     data-target="#navbarResponsive"
                     aria-controls="navbarResponsive"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="fas fa-bars"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link active" to="/">
+                    aria-expanded="true"
+                    aria-label="Toggle navigation"> <span className="fas fa-bars"></span> </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto "  id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+
+          <Nav.Link className="nav-link active" to="/">
                                 Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/about">
+                            </Nav.Link>
+            </li>
+           
+          <li class="nav-item">
+
+          <Nav.Link className="nav-link" to="/about">
                                 About
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/services">
+                            </Nav.Link>
+            </li>
+            <li class="nav-item">
+
+            <Nav.Link className="nav-link" to="/services">
                                 Services
-                            </Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdownPortfolio"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Portfolio
-                            </a>
-                            <div
-                                className="dropdown-menu dropdown-menu-right"
-                                aria-labelledby="navbarDropdownPortfolio"
-                            >
-                                <a
-                                    className="dropdown-item"
-                                    href="portfolio-1-col.html"
-                                >
-                                    1 Column Portfolio
-                                </a>
-                                <a
-                                    className="dropdown-item"
-                                    href="portfolio-2-col.html"
-                                >
-                                    2 Column Portfolio
-                                </a>
-                                <a
-                                    className="dropdown-item"
-                                    href="portfolio-3-col.html"
-                                >
-                                    3 Column Portfolio
-                                </a>
-                                <a
-                                    className="dropdown-item"
-                                    href="portfolio-4-col.html"
-                                >
-                                    4 Column Portfolio
-                                </a>
-                                <a
-                                    className="dropdown-item"
-                                    href="portfolio-item.html"
-                                >
-                                    Single Portfolio Item
-                                </a>
-                            </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdownBlog"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Blog
-                            </a>
-                            <div
-                                className="dropdown-menu dropdown-menu-right"
-                                aria-labelledby="navbarDropdownBlog"
-                            >
-                                <a
-                                    className="dropdown-item"
-                                    href="blog-home-1.html"
-                                >
-                                    Blog Home 1
-                                </a>
-                                <a
-                                    className="dropdown-item"
-                                    href="blog-home-2.html"
-                                >
-                                    Blog Home 2
-                                </a>
-                                <a
-                                    className="dropdown-item"
-                                    href="blog-post.html"
-                                >
-                                    Blog Post
-                                </a>
-                            </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdownBlog"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Pages
-                            </a>
-                            <div
-                                className="dropdown-menu dropdown-menu-right"
-                                aria-labelledby="navbarDropdownBlog"
-                            >
-                                <a className="dropdown-item" href="faq.html">
-                                    FAQ
-                                </a>
-                                <a className="dropdown-item" href="404.html">
-                                    404
-                                </a>
-                                <a
-                                    className="dropdown-item"
-                                    href="pricing.html"
-                                >
-                                    Pricing Table
-                                </a>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/contact">
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+                            </Nav.Link>
+            </li>
+            <li class="nav-item">
+
+<Nav.Link className="nav-link" to="/portfolio">
+                    Portfolio
+                </Nav.Link>
+</li> 
+<li class="nav-item">
+
+<Nav.Link className="nav-link" to="/blog">
+                    Blog
+                </Nav.Link>
+</li>
+           
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item to='/faq'>FAQ</NavDropdown.Item>
+              <NavDropdown.Item to='/pricing'>
+Pricing              </NavDropdown.Item>
+              
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            </ul>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  );
 }
+
+export default Navigation;
+
