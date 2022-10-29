e69bf8c6441923601c42534c93e63306e407b3d8
VeroniqueDM <veronika.dmiteva@gmail.com>
2022-10-29 18:00:39 +0200

    fixed routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
     
     <Navbar bg="light" expand="lg" className="navbar fixed-top navbar-expand-lg navbar-dark bg-light top-nav fixed-top">
       <Container className="container">
-        <Navbar.Brand className="navbar-brand" to="/"> 
+        <Navbar.Brand  as={Link} className="navbar-brand" to="/"> 
         <img className="nav-logo"
                         src="images//footer-logo.png"
                         alt="logo"
@@ -26,46 +27,52 @@ function Navigation() {
           <ul class="navbar-nav ml-auto">
           <li class="nav-item">
 
-          <Nav.Link className="nav-link active" to="/">
+          <Nav.Link  as={Link} className="nav-link active" to="/">
                                 Home
                             </Nav.Link>
             </li>
            
           <li class="nav-item">
 
-          <Nav.Link className="nav-link" to="/about">
+          <Nav.Link as={Link} className="nav-link" to="/about">
                                 About
                             </Nav.Link>
             </li>
             <li class="nav-item">
 
-            <Nav.Link className="nav-link" to="/services">
+            <Nav.Link  as={Link} className="nav-link" to="/services">
                                 Services
                             </Nav.Link>
             </li>
             <li class="nav-item">
 
-<Nav.Link className="nav-link" to="/portfolio">
+<Nav.Link  as={Link} className="nav-link" to="/portfolio">
                     Portfolio
                 </Nav.Link>
 </li> 
 <li class="nav-item">
 
-<Nav.Link className="nav-link" to="/blog">
+<Nav.Link  as={Link} className="nav-link" to="/blog">
                     Blog
                 </Nav.Link>
 </li>
            
             <NavDropdown title="More" id="basic-nav-dropdown">
-              <NavDropdown.Item to='/faq'>FAQ</NavDropdown.Item>
-              <NavDropdown.Item to='/pricing'>
+              <NavDropdown.Item  as={Link} to='/faq'>FAQ</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to='/pricing'>
 Pricing              </NavDropdown.Item>
               
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item  as={Link} href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+            <li class="nav-item">
+
+<Nav.Link  as={Link} className="nav-link" to="/contact">
+                    Contact
+                </Nav.Link>
+</li> 
             </ul>
           </Nav>
         </Navbar.Collapse>
